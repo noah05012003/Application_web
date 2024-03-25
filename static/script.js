1f5f908c879d1890e1d741fb55e85960fe8fbd41
noah05012003 <1noah0501@gmail.com>
2024-03-25 02:02:43 -0400

    boom

--- static/script.js
+++ static/script.js
@@ -1,1 +1,11 @@
-alert('Bonjour')
+const apiBaseUrl = "https://api.rawg.io/api/games";
+const apiKey = "86a34209259b4dd496f0989055c1711b";
+
+async function fetchGamesNowPlaying() {
+    const response = await fetch(`${apiBaseUrl}/https://api.rawg.io/api/games?key=${apiKey}`);
+    const jsonResponse = await response.json();
+    const movies = jsonResponse.results;
+    console.log(movies);
+}
+
+fetchGamesNowPlaying();
