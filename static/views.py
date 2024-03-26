9e4109bf0c550b20747a6f8fb31de6852ea162b7
Ujojo <johnsonuwe@gmail.com>
2024-03-26 02:16:40 -0400

    Commit -Ajout des routes

--- static/views.py
+++ static/views.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint , render_template
 
 views = Blueprint('views',__name__)
+
+@views.route('/following')
+def following():
+    return render_template("following.html")
+
+@views.route('/library')
+def library():
+    return render_template("library.html")
+
+@views.route('/genre')
+def genre():
+    return render_template("genre.html")
+
+@views.route('/platforms')
+def platforms():
+    return render_template("platforms.html")
+
